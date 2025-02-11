ad14a59662decd742d7528f498c9bbd8d3fe0ea1
Tashrif-007 <tashrifpro@gmail.com>
2025-02-12 03:27:36 +0600

    doing snippetdetail page

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -107,3 +107,21 @@ export const starSnippet = mutation({
         }
     }
 })
+
+export const getSnippetById = query({
+    args: { snippetId: v.id("snippets")},
+    handler: async (ctx,args) => {
+        const snippet = await ctx.db.get(args.snippetId);
+        if(!snippet) throw new Error("Snippet not found");
+
+        return snippet;
+    },
+})
+
+export const getComments = query({
+    args: {snippetid: v.id("snippets")},
+    handler: async(ctx,args) => {
+        const comments = await ctx.db.query("snippetComments").withIndex("by_snippet_id").filter((q)=>q.eq(q.field("snippetId"),args.snippetid)).order("desc").collect();
+        return comments;
+    }
+})
